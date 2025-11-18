3759375e5137bc5dbcc19ab999df449d08910ebd
Mirage2000cc <116649520+Mirage2000cc@users.noreply.github.com>
2025-11-18 09:26:51 -0300

    70 rutas hijas

---
+++ src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from "@angular/router";
+
+@Component({
+  selector: 'dashboard-page',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './dashboard-page.component.html',
+  styleUrl: './dashboard-page.component.css'
+})
+export  default class DashboardPageComponent {
+
+}
