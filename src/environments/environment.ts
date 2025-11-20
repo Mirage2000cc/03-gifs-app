a2bbb434e898b5659e464833bc1728877cfc09e9
Mirage2000cc <116649520+Mirage2000cc@users.noreply.github.com>
2025-11-20 17:10:43 -0300

    81

---
+++ src/environments/environment.ts
@@ -0,0 +1,22 @@
+export const environment = {
+  production: true,
+  companyName: 'Gifs',
+  companyName2: 'App',
+  companySlogan: 'Find the best gifs here',
+
+
+
+
+
+
+// Apikeys
+giphyApiKey: 'iiZySihSXN9DW2TsInF7MpHBRFNiltQG',
+
+};
+
+
+
+
+
+
+
